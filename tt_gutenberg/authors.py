013b06be30dc746879d1faf01bfc70ae258def91
shsonavane <shsona@iu.edu>
2025-12-05 23:24:28 -0500

    made required corrections

--- tt_gutenberg/authors.py
+++ tt_gutenberg/authors.py
@@ -1,26 +1,27 @@
 import pandas as pd
 from tt_gutenberg.utils import load_gutenberg_data
 
+
 def list_authors(by_languages=True, alias=True):
-    """
-    Return author aliases sorted by translation count across languages.
-    """
-    df = load_gutenberg_data()
-
-    # Drop missing authors
-    df = df.dropna(subset=["alias"])
-
-    # Compute translation count per author (unique languages)
-    translation_counts = (
-        df.groupby(["gutenberg_author_id", "alias"])["language"]
-          .nunique()
-          .reset_index(name="translation_count")
+    base_url = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/"
+    
+    df_authors = pd.read_csv(base_url + "gutenberg_authors.csv")
+    df_metadata = pd.read_csv(base_url + "gutenberg_metadata.csv")
+    df_languages = pd.read_csv(base_url + "gutenberg_languages.csv")
+    
+    df_merged = pd.merge(df_authors, df_metadata, on='gutenberg_id', how='inner')
+    
+    df_merged = pd.merge(df_merged, df_languages, on='gutenberg_id', how='inner')
+    
+    df_merged = df_merged.dropna(subset=["alias"])
+    
+    df_merged["alias_count"] = df_merged["aliases"].fillna("").apply(
+        lambda x: len(str(x).split("/"))
     )
-
-    # Sort by translation count (descending)
-    df_sorted = translation_counts.sort_values("translation_count", ascending=False)
-
+    
+    df_sorted = df_merged.sort_values("alias_count", ascending=False)
+    
     if alias:
         return df_sorted["alias"].tolist()
     else:
-        return df_sorted.head(20)
+        return df_sorted
